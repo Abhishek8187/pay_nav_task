37a8ac185a3217ed3139765ec493d36abc0ee834
Abhishek Kannaujia <abhishekkannaujia96@gmail.com>
2023-07-24 19:39:53 +0530

    rank display

---
+++ lib/constants/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color arrow_up = Color(0xFF8EFFAA);
+Color arrow_down = Color(0xFFFF4155);
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pay_nav_task/constants/colors.dart';
 import 'package:pay_nav_task/utils/general_container.dart';
 
 class HomePage extends StatefulWidget {
@@ -26,7 +27,8 @@ class _HomePageState extends State<HomePage> {
                     child: Image.asset('images/Ellipse2.png')),
                 Image.asset('images/ellipse1.png'),
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
                   child: Row(
                     //mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -98,12 +100,156 @@ class _HomePageState extends State<HomePage> {
                       ],
                     ),
                   ),
+                ),
+                Positioned(
+                    right: 0,
+                    bottom: 5,
+                    child: Image.asset('images/Ellipse3.png')),
+                Padding(
+                  padding:
+                      EdgeInsets.only(top: height * 0.2, left: 15, right: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      RankHolders(
+                        rank: const Text(
+                          '2',
+                          style: TextStyle(
+                              color: Color(0xFFBBB2D9),
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18,
+                              fontFamily: 'Plus Jakarta Sans'),
+                        ),
+                        height: height * 0.11,
+                        width: height * 0.11,
+                        borderRadius: 80,
+                        gradient: const LinearGradient(
+                            colors: [
+                              Color(0xFF4B19C3),
+                              Color(0xFF1F0293),
+                              Color(0xFF1B0290)
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter),
+                        points: 7260,
+                        name: 'Natasha Chowdary',
+                        icon: Icon(
+                          Icons.arrow_drop_up,
+                          color: arrow_up,
+                        ),
+                      ),
+                      RankHolders(
+                        rank:  Image.asset('images/rank1.png'),
+                        height: height * 0.17,
+                        width: height * 0.17,
+                        borderRadius: 100,
+                        gradient:  LinearGradient(
+                            colors: [
+                              Color(0xFF2A12CCCC),
+                              Color(0xFF1A018BCC),
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter),
+                        points: 8370,
+                        name: 'Raja Reddy',
+                        icon: SizedBox(),
+                      ),
+
+                      RankHolders(
+                        rank: const Text(
+                          '3',
+                          style: TextStyle(
+                              color: Color(0xFFBBB2D9),
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18,
+                              fontFamily: 'Plus Jakarta Sans'),
+                        ),
+                        height: height * 0.11,
+                        width: height * 0.11,
+                        borderRadius: 60,
+                        gradient:  LinearGradient(
+                            colors: [
+                              Color(0xFF4F5BEC),
+                              Color(0xFF3542DF00).withOpacity(0),
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter),
+                        points: 6260,
+                        name: 'Samvibhan Singh',
+                        icon: Icon(
+                          Icons.arrow_drop_down,
+                          color: arrow_down,
+                        ),
+                      ),
+
+                    ],
+                  ),
                 )
               ],
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class RankHolders extends StatelessWidget {
+  final Widget rank;
+  final double height;
+  final double width;
+  final double borderRadius;
+  final LinearGradient gradient;
+  final int points;
+  final String name;
+  final Widget icon;
+
+  RankHolders({
+    required this.rank,
+    required this.height,
+    required this.width,
+    required this.borderRadius,
+    required this.gradient,
+    required this.points,
+    required this.name,
+    required this.icon,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        rank,
+        icon,
+        Container(
+          height: height,
+          width: width,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(borderRadius),
+              gradient: gradient),
+          child: Center(child: Placeholder()),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          points.toString(),
+          style: TextStyle(
+              color: Color(0xFF99B6FF),
+              fontWeight: FontWeight.w300,
+              fontSize: 25,
+              fontFamily: 'Plus Jakarta Sans'),
+        ),
+        Text(
+          name,
+          style: TextStyle(
+              color: Color(0xFFFFFFFF),
+              fontWeight: FontWeight.w300,
+              fontSize: 12,
+              fontFamily: 'Plus Jakarta Sans'),
+        ),
+      ],
     );
   }
 }
